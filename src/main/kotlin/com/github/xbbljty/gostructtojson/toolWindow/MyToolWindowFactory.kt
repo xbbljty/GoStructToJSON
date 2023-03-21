91d5b8fdef5e7acceece244cc69b9bc224c16720
GitHub Action <action@github.com>
2023-03-21 07:39:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/xbbljty/gostructtojson/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/xbbljty/gostructtojson/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.xbbljty.gostructtojson.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.xbbljty.gostructtojson.MyBundle
+import com.github.xbbljty.gostructtojson.services.MyProjectService
 import javax.swing.JButton
 
 
